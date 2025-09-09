7c9eedc99be021c88d7efbbbb99549d806444f02
yuheng2002 <yuheng.zhang2002@gmail.com>
2025-09-09 01:04:09 -0700

    Create 1431. Kids With the Greatest Number of Candies.py

---
+++ easy/1431. Kids With the Greatest Number of Candies.py
@@ -0,0 +1,20 @@
+"""
+LeetCode 1431: Kids With the Greatest Number of Candies
+Link: https://leetcode.com/problems/kids-with-the-greatest-number-of-candies/
+Approach 1: Calculate the candies each kid has after adding the extra candies and then compare it with all the others with a for loop (my original thought)
+Time: O(n)^2
+"""
+
+class Solution:
+    def kidsWithCandies(self, candies: List[int], extraCandies: int) -> List[bool]:
+        lst = []
+        for i in range(len(candies)):
+            temp_candy = candies[i] + extraCandies
+            max = temp_candy
+            for j in range (len(candies)):
+                if candies[j] > temp_candy:
+                    max = candies[j]
+            lst.append(temp_candy == max)
+        return lst
+
+  
